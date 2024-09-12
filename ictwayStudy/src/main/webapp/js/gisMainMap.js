96c044b90adaec7c4fd102a30923961c3731434a
crystal <sjk0868@naver.com>
2024-09-12 17:38:36 +0900

    시군구, 대학 wfs로 표현

--- ictwayStudy/src/main/webapp/js/gisMainMap.js
+++ ictwayStudy/src/main/webapp/js/gisMainMap.js
@@ -1,21 +1,38 @@
-
+//http://192.168.0.2:8080/geoserver/cite/wms
 document.addEventListener('DOMContentLoaded', function(){
-	var vector = new ol.layer.Vector({});
+	var vectorUV = new ol.layer.Vector({});
+	var vectorSeoul = new ol.layer.Vector({});
 	var geoJson = new ol.format.GeoJSON({
     	projection: 'EPSG:3857'
 	});
-	var vectorSource = new ol.source.Vector({});
+	var vectorSourceUV = new ol.source.Vector({});
+	var vectorSourceSeoul = new ol.source.Vector({});
 	
-	$.get('http://localhost:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:ctprvn&maxFeatures=50&outputFormat=application/json&srsname=EPSG:3857'
+	$.get('http://192.168.0.2:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:xsdb_uv_poi_tm&maxFeatures=50&outputFormat=application/json&srsname=EPSG:3857'
 	    // success callback 
 	    ,function (data) {
 	        // first add features  
-	        vectorSource.addFeatures(geoJson.readFeatures(data));
+	        vectorSourceUV.addFeatures(geoJson.readFeatures(data));
 	
 	        // then display map...
-	        vector.setSource(vectorSource);   
-	        vector.setStyle(getFeature());  
-	        vector.setVisible(true);                     
+	        vectorUV.setSource(vectorSourceUV);   
+	        vectorUV.setStyle(getUVFeature());  
+	        vectorUV.setVisible(true);                     
+	   }
+	);
+	
+	$.get('http://192.168.0.2:8080/geoserver/cite/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cite:ctprvn&maxFeatures=50&outputFormat=application/json&srsname=EPSG:3857'
+	    // success callback 
+	    ,function (data) {
+			
+	        // first add features  
+	        vectorSourceSeoul.addFeatures(geoJson.readFeatures(data));
+			for(var i in vectorSourceSeoul.getFeatures()) {
+				vectorSourceSeoul.getFeatures()[i].setStyle(getSeoulFeature(vectorSourceSeoul.getFeatures()[i].values_.ctprvn_cd));		
+			}
+	        // then display map...
+	        vectorSeoul.setSource(vectorSourceSeoul);   
+	        vectorSeoul.setVisible(true);                     
 	   }
 	);
 
@@ -27,16 +44,30 @@ document.addEventListener('DOMContentLoaded', function(){
 			/*시군구wms*/
 			new ol.layer.Tile({
 				source : new ol.source.TileWMS({
-					url : 'http://localhost:8080/geoserver/cite/wms',
+					url : 'http://192.168.0.2:8080/geoserver/cite/wms',
 					params : {
-						'SRS' : 'EPSG:5186',
+						'SRS' : 'EPSG:3857',
 						'LAYERS' : 'cite:ctprvn'
 					},
 					serverType : 'geoserver'
 				}),
 				visible : false
 			}),
-			vector
+			/*대학wms*/
+			new ol.layer.Tile({
+				source : new ol.source.TileWMS({
+					url : 'http://192.168.0.2:8080/geoserver/cite/wms',
+					params : {
+						'SRS' : 'EPSG:3857',
+						'LAYERS' : 'cite:xsdb_uv_poi_tm'
+					},
+					serverType : 'geoserver'
+				}),
+				visible : false
+			}),
+			vectorSeoul,
+			vectorUV
+			
 		],
 		target: 'mainMap',
 		keyboardEventTarget: document,
@@ -50,10 +81,35 @@ document.addEventListener('DOMContentLoaded', function(){
 	map.addControl(zoomSlider);
 });
 
-function getFeature () {
+function getUVFeature () {
 	return new ol.style.Style({
-		fill: new ol.style.Fill({
-			      color: 'rgba(100, 149, 237, 0.6)'
-			    })
+		 image : new ol.style.Icon({
+		    src : 'images/gis/university.png'
+		 })
 	});
+}
+
+function getSeoulFeature (ctprvn_cd) {
+	if(ctprvn_cd == '11' || (ctprvn_cd >= '26' && ctprvn_cd <= '31')) {
+		return new ol.style.Style({
+	        stroke: new ol.style.Stroke({
+				color: 'rgba(255, 0, 0, 1)',
+				width: 2
+			}),
+			fill: new ol.style.Fill({
+				color: 'rgba(255, 0, 0, 0.6)'
+			})
+		});
+			
+	} else {
+		return new ol.style.Style({
+	        stroke: new ol.style.Stroke({
+				color: 'rgba(100, 149, 237, 1)',
+				width: 2
+			}),
+			fill: new ol.style.Fill({
+				color: 'rgba(100, 149, 237, 0.6)'
+			})
+		});
+	}
 }
